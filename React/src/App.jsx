86de89539df0021125ccf264e4b1dac40f0bf8a5
vishnu-priya-17 <108928013+vishnu-priya-17@users.noreply.github.com>
2023-03-26 14:52:30 +0530

    Add files via upload

---
+++ React/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Team from "./react4/Team"
+export class App extends React.Component {
+    render()
+    {
+        return (
+        <div>
+        <Team/>
+        </div>
+        );
+    }
+}
+
+export default App;
---
+++ React/src/react4/Team.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Team()
+{
+    return (
+        <div>This is Team Functional Component</div>
+    );
+}
+
+export default Team;
